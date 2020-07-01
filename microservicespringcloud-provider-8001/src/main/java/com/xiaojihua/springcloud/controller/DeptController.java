6ecce4a416ffae26fd1f620a5f54aedc6ea41109
xiaojihua <240971505@qq.com>
2020-07-01 17:53:33 +0800

    20200701

--- microservicespringcloud-provider-8001/src/main/java/com/xiaojihua/springcloud/controller/DeptController.java
+++ microservicespringcloud-provider-8001/src/main/java/com/xiaojihua/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.xiaojihua.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,9 @@ import com.xiaojihua.springcloud.service.DeptService;
 public class DeptController {
 	@Autowired
 	private DeptService service;
+	
+	@Autowired
+	private DiscoveryClient client;
 
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept) {
@@ -31,4 +36,24 @@ public class DeptController {
 	public List<Dept> list() {
 		return service.list();
 	}
+	
+	/**
+	  *   服务发现
+	 * @return
+	 */
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		//微服务列表
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+		//找到指定微服务，并打印相关信息
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
+
 }
